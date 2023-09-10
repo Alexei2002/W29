10d24129966b0e77c4a36ff77e53b45bd24b1735
Alex <konyukhov94@list.ru>
2023-09-10 12:10:30 +0300

    wqц22

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,65 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        int year = 2020; // год для проверки
+        if (isLeapYear(year)) {
+            System.out.println(year + " год является високосным");
+        } else {
+            System.out.println(year + " год не является високосным");
+        }
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        int currentYear = LocalDate.now().getYear();
+        int deviceOS = 1; // предположим, что устройство работает на Android
+        int deviceReleaseYear = 2018; // год выпуска устройства
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+        String appVersion = getAppVersion(deviceOS, deviceReleaseYear, currentYear);
+        System.out.println("Установить пользователю: " + appVersion);
+
+        int deliveryDistance = 95; // дистанция до клиента
+
+        int deliveryDays = calculateDeliveryDays(deliveryDistance);
+
+        if (deliveryDays > 0) {
+            System.out.println("Потребуется дней: " + deliveryDays);
+        } else {
+            System.out.println("Доставка невозможна.");
+        }
+    }
+
+    public static boolean isLeapYear(int year) {
+        return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
+    }
+
+    public static String getAppVersion(int osType, int releaseYear, int currentYear) {
+        if (releaseYear > currentYear) {
+            if (osType == 0) {
+                return "Облегченную версию для iOS";
+            } else if (osType == 1) {
+                return "Облегченную версию для Android";
+            } else {
+                return "Неподдерживаемая операционная система";
+            }
+        } else {
+            if (osType == 0) {
+                return "Обычную версию для iOS";
+            } else if (osType == 1) {
+                return "Обычную версию для Android";
+            } else {
+                return "Неподдерживаемая операционная система";
+            }
+        }
+    }
+
+    public static int calculateDeliveryDays(int distance) {
+        if (distance <= 20) {
+            return 1;
+        } else if (distance <= 60) {
+            return 2;
+        } else if (distance <= 100) {
+            return 3;
+        } else {
+            return -1; // доставка невозможна
         }
     }
 }
